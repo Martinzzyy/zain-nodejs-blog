b13246afcb374ff8bade6b1e852d8b029bb853f4
Arvin <15038460047@163.com>
2017-07-01 21:09:32 +0800

    11

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+
 var config = require('./models/baseconfig');
 var LabelsHelper = require('./models/labels');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 // view engine setup
@@ -40,8 +41,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
 
+app.get('/zain/login',function(req, res){
+  res.render('zain_login');
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
