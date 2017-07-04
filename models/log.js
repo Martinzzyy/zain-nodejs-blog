8347807b9a5b2d5ed9c38a0a48d68f5e88141e1d
arvin <15038460047@163.com>
2017-07-04 14:33:00 +0800

    11

--- models/log.js
+++ models/log.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 
 module.exports = function(log){
-    var d = new Date(),file = d.getFullYear()+'-'+d.getMonth()+'-'+d.getDate()+'.log';
-    log = d.toISOString()+'：'+log;
+    var d = new Date(),file = '../logs/'+d.getFullYear()+'-'+d.getMonth()+'-'+d.getDate()+'.log';
+    log = d.toISOString()+'：'+log + '\r\n';
     fs.appendFileSync(file,log)
 }
